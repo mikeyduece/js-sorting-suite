d39e956e8fdf8826f8018a7e1491889fccccd2af
Mike Heft <mikeheft@gmail.com>
2017-11-24 15:22:06 -0700

    ugh

--- mergeSort.js
+++ mergeSort.js
@@ -2,28 +2,25 @@ const pry = require('pryjs');
 
 function mergeSort(sugArray) {
   if(sugArray.length <= 1){
-    return sugArray
+    return sugArray;
   }else {
     var mid = (sugArray.length / 2);
     var left = mergeSort(sugArray.slice(0,mid));
     var right = mergeSort(sugArray.slice(mid, sugArray.length + 1));
     return merge(left, right);
-    eval(pry.it);
   }
 }
 
 function merge(left, right) {
   if(left.length < 1) {
-    return right
+     right;
   }else if(right.length < 1){
-    return left
+     left;
   }else if(left[0] < right[0]){
-    return [left[0]].push(merge(left.slice(1,left.length), right));
+     [left[0]].push(merge(left.slice(1,left.length), right));
   }else{
-    eval(pry.it);
-    return [right[0]].push(merge(left, right.slice(1,right.length)));
+     [right[0]].push(merge(left, right.slice(1,right.length)));
   }
-  eval(pry.it);
 }
 
 module.exports = mergeSort

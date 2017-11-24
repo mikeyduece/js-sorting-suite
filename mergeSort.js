f1f94b0c725590bbd75ce8c07fc30be900d185a1
Mike Heft <mikeheft@gmail.com>
2017-11-24 15:12:27 -0700

    More work on merge

--- mergeSort.js
+++ mergeSort.js
@@ -7,22 +7,23 @@ function mergeSort(sugArray) {
     var mid = (sugArray.length / 2);
     var left = mergeSort(sugArray.slice(0,mid));
     var right = mergeSort(sugArray.slice(mid, sugArray.length + 1));
-    eval(pry.it);
     return merge(left, right);
+    eval(pry.it);
   }
 }
 
 function merge(left, right) {
-  eval(pry.it);
   if(left.length < 1) {
     return right
   }else if(right.length < 1){
     return left
   }else if(left[0] < right[0]){
-    return ([left[0]] + merge(left[1..left.length], right))
+    return [left[0]].push(merge(left.slice(1,left.length), right));
   }else{
-    return ([right[0]] + merge(left, right[1..right.length]))
+    eval(pry.it);
+    return [right[0]].push(merge(left, right.slice(1,right.length)));
   }
+  eval(pry.it);
 }
 
 module.exports = mergeSort

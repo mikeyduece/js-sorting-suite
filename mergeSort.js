905757465a132eb764277fb682ff5ee14c83fa07
Mike Heft <mikeheft@gmail.com>
2017-11-25 14:59:14 -0700

    I did the thing

--- mergeSort.js
+++ mergeSort.js
@@ -7,21 +7,23 @@ function mergeSort(sugArray) {
     var mid = (sugArray.length / 2);
     var left = sugArray.slice(0,mid);
     var right = sugArray.slice(mid, sugArray.length);
-  eval(pry.it);
-    return merged(mergeSort(left), mergeSort(right));
+    return merge(mergeSort(left), mergeSort(right));
   }
 }
 
-function merged(left, right) {
+function merge(left, right) {
   var stackSorted = [];
-  if(left.length) {
-     Array.prototype.push.apply(stackSorted, left);
-  }else if(right.length){
-     Array.prototype.push.apply(stackSorted, right);
-  }else if(left[0] < right[0]){
-    stackSorted.push(left.shift());
-  }else{
-    stackSorted.push(right.shift());
+  while (left.length && right.length) {
+    if(left[0] <= right[0]){
+      stackSorted.push(left.shift());
+    }else{
+      stackSorted.push(right.shift());
+    }
+  }
+  if (left.length) {
+    Array.prototype.push.apply(stackSorted, left);
+  }else if (right.length){
+    Array.prototype.push.apply(stackSorted, right);
   }
   return stackSorted
 }
